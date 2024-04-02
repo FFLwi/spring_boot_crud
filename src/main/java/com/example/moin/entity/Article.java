f947fe1f7223cf4a1326306537dc31e794164d02
dean <wi04707@gmail.com>
2024-04-03 04:26:38 +0900

    start_mission_m

--- src/main/java/com/example/moin/entity/Article.java
+++ src/main/java/com/example/moin/entity/Article.java
@@ -5,13 +5,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Entity //DB 가 해당 객체를 인식 가능!
 @AllArgsConstructor
+@NoArgsConstructor //디폴트 생성자
 @ToString
+@Getter
 public class Article {
-
     @Id //대표값을 지정!
     @GeneratedValue // 1,2,3,4  자동 생성 어노테이션 !
     private Long id;
--- src/main/java/com/example/moin/repository/ArticleRepository.java
+++ src/main/java/com/example/moin/repository/ArticleRepository.java
@@ -3,6 +3,9 @@ package com.example.moin.repository;
 import com.example.moin.entity.Article;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ArticleRepository extends CrudRepository<Article, Long> {
+import java.util.ArrayList;
 
+public interface ArticleRepository extends CrudRepository<Article, Long> {
+    @Override
+    ArrayList<Article> findAll();
 }
