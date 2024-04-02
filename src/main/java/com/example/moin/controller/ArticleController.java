21769e92397b1eb9fd3a5e7add38b7b718b52b6a
dean <wi04707@gmail.com>
2024-04-03 00:45:09 +0900

    after-lombok-refactory

--- src/main/java/com/example/moin/controller/ArticleController.java
+++ src/main/java/com/example/moin/controller/ArticleController.java
@@ -3,12 +3,14 @@ package com.example.moin.controller;
 import com.example.moin.dto.ArticleForm;
 import com.example.moin.entity.Article;
 import com.example.moin.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Slf4j //로깅을 위한 어노테이션 골뱅이
 public class ArticleController {
     @Autowired //스프링부트가 미리 생성해놓은 객체를 가져가다 자동연결!
     private ArticleRepository articleRepository;
@@ -20,14 +22,15 @@ public class ArticleController {
 
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form){
-        System.out.println(form.toString());
-
+        //System.out.println(form.toString()); -> 로깅으로 대체!
+        log.info(form.toString());
         //1. Dto를 변환! to Entity!
         Article article = form.toEntity();
 
         //2. Repository에게 Entity를 DB안에 저장하게 함
         Article saved = articleRepository.save(article);
-        System.out.println(saved.toString());
+        log.info(saved.toString());
+        //System.out.println(saved.toString());
 
         return "";
     }
